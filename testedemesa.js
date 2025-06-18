cde112a83018f9fef09518796ea4a9075563cadd
ANA PRISCILA PAULINO COSTA DE SOUZA <ana.paulino@aluno.senac.rio>
2025-06-18 11:52:29 -0300

    adiconei novo commit

---
+++ testedemesa.js
@@ -0,0 +1,21 @@
+//var a = 5;
+//var b = 7;
+
+//for(i=0; i<=5; i++){
+    //if(b < i){
+     //   console.log(a);
+    //}
+   // a=i-1;
+    //b--;
+//} saida 2,3.
+
+//desenvolver algoritimos, if e loop
+
+//let x = 5;
+//let y = 10;
+
+//for(i=10; i>10; i--){ 
+    //console.log(x-y); // não executa pois não está dentro do for = i >10'
+//} // não tem saida porque nenhum numero dentro do for é > que 10.
+
+//console.log(i);// não está dentro do for e não imprime em outras linguagens só no javascript.
---
+++ while.js
@@ -0,0 +1,21 @@
+//let i = 0 // o zero é na maioria das vezes o primeiro elemento. // i = iteração
+
+//while(i < 6){
+   // console.log(i) While
+    //i++
+//}
+
+
+//let j = Do while
+
+//do{
+    //console.log(j)
+    //j++
+//}while (j<6) {
+    
+//}
+
+for(k=0;k<6;k++){ // for
+console.log(k)
+
+}
